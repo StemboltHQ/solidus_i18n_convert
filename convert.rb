d1e802350d000242423b6e9b44e13d5cfd703f62
Luuk Veenis <luukveenis@gmail.com>
2016-05-09 13:51:40 -0700

    Rename unwrap -> hash_flatten

--- convert.rb
+++ convert.rb
@@ -2,11 +2,11 @@ require 'yaml'
 require 'pp'
 
 # Flatten hash by joining nested keys with periods and assigning the same value
-def unwrap prefix, value
+def hash_flatten prefix, value
   if value.is_a? Hash
     value.flat_map do |k, v|
       key = prefix.empty? ? k : [prefix, k].join('.')
-      unwrap(key, v)
+      hash_flatten(key, v)
     end
   else
     [[prefix, value]]
@@ -16,8 +16,8 @@ end
 source = YAML.load_file("locales/en.yml")
 destination = YAML.load_file("locales/nl.yml")
 
-flat_source = unwrap('', source['en']).to_h
-flat_dest = unwrap('', destination['nl']).to_h
+flat_source = hash_flatten('', source['en']).to_h
+flat_dest = hash_flatten('', destination['nl']).to_h
 
 missing_keys = flat_source.keys - flat_dest.keys
 
