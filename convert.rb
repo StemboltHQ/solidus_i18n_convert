f5a178d10f2f94260de04fd1f7efb11e7b9e87e0
Luuk Veenis <luukveenis@gmail.com>
2016-05-10 07:18:02 -0700

    Provide list of target locales to run against

--- convert.rb
+++ convert.rb
@@ -2,6 +2,9 @@ require 'active_support'
 require 'yaml'
 require 'pp'
 
+# Locales to be converted
+TARGET_LOCALES = ["fr", "nl"]
+
 # Flatten hash by joining nested keys with periods and assigning the same value
 def hash_flatten prefix, value
   if value.is_a? Hash
@@ -26,28 +29,30 @@ def reconstruct flat_hash
   end
 end
 
-dest_locale = "nl"
 source = YAML.load_file("locales/en.yml")
-destination = YAML.load_file("locales/#{dest_locale}.yml")
-
 flat_source = hash_flatten('', source['en']).to_h
-flat_dest = hash_flatten('', destination[dest_locale]).to_h
 
-missing_keys = flat_source.keys - flat_dest.keys
+TARGET_LOCALES.each do |target_locale|
+  destination = YAML.load_file("locales/#{target_locale}.yml")
+  flat_dest = hash_flatten('', destination[target_locale]).to_h
 
-missing_keys.each do |key|
-  missing_value = flat_source[key]
-  candidates = flat_source.select { |k, v| v == missing_value }.keys
-  candidates -= missing_keys
-  candidates.sort_by!(&:length)
-  next if candidates.empty?
+  missing_keys = flat_source.keys - flat_dest.keys
 
-  flat_dest[key] ||= flat_dest[candidates.first]
-end
+  missing_keys.each do |key|
+    missing_value = flat_source[key]
+    candidates = flat_source.select { |k, v| v == missing_value }.keys
+    candidates -= missing_keys
+    candidates.sort_by!(&:length)
+    next if candidates.empty?
 
-new_locale = reconstruct(flat_dest)
-File.open('nl_new.yml', 'w') do |f|
-  yaml = YAML.dump({dest_locale => new_locale}, line_width: 1000)
-  yaml.gsub!(/ +$/, '')
-  f.write(yaml)
+    flat_dest[key] ||= flat_dest[candidates.first]
+  end
+
+  new_locale = reconstruct(flat_dest)
+  FileUtils.mkdir_p "output"
+  File.open("output/#{target_locale}_new.yml", 'w') do |f|
+    yaml = YAML.dump({target_locale => new_locale}, line_width: 1000)
+    yaml.gsub!(/ +$/, '')
+    f.write(yaml)
+  end
 end
